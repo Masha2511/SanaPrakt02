87fcfcad3296fdc0b6c4554891678b3a3289f70d
Maria Evdokymova <sana22_yemo@student.ztu.edu.ua>
2022-11-10 12:28:34 +0200

    changed the text

--- SanaPrakt02/Task1/Program.cs
+++ SanaPrakt02/Task1/Program.cs
@@ -1,6 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
-Console.WriteLine("Курси Sana Commerce Software Development Bootcamp");
+Console.WriteLine("Hello");
+Console.WriteLine("My name");
+Console.WriteLine("Is Maria.");
+Console.WriteLine("I have");
+Console.WriteLine("A dog");
